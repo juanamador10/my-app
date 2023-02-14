9936312aef7443c5d4213b79af37807e54a6d2c2
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2023-02-14 11:49:41 -0600

    Nombre de Projecto: my-app. Descripcion: Version del proyecto my-app

---
+++ src/main/java/com/juan/myapp/entity/TestEntity.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.juan.myapp.entity;
+
+/**
+ * @author Juan Fernando Amador Miranda
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilacion de archivo testentity
+	 * */
+	
+		public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+		private String nombre;
+}
